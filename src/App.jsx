497e13b6bb813e7366b9536c1e3a066214ef0a0e
Emanuel <emanuelmhlanga55@gmail.com>
2026-01-16 10:22:08 +0200

    can generate random binary trees now

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { ReactFlow, Background, Controls,  ReactFlowProvider } from '@xyflow/react';
+import { useState, useCallback } from 'react';
+import { ReactFlow, Background, Controls,  ReactFlowProvider, applyEdgeChanges, applyNodeChanges } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 
 const initialNodes = [
@@ -25,6 +25,73 @@ const initialEdges = [
     label: 'connects with',
   },
 ];
+
+
+// Binary Tree Generator
+function binaryGen(maxDepth = 2, maxChildren = 2){
+  const nodes = [];
+  const edges = [];
+
+  // Used to give every node a unique id
+  let idCounter = 0; 
+
+  function createNode(x, y, depth, parentId= null){
+    const id = `${idCounter++}`;
+
+    // add current node to the nodes array
+    nodes.push({
+      id,
+      position: {x, y},
+      data: {  label: Math.floor(Math.random() * 100) },
+    });
+
+    // If this node has a parent than connect parent to child
+    if (parentId !== null){
+      edges.push({
+        id: `e${parentId}-${id}`,
+        source: parentId,
+        target: id,
+      })
+    }
+
+     if (depth >= maxDepth || Math.random() < 0.3) return;
+
+     
+    const childCount = Math.floor(Math.random() + (maxChildren + 1));
+    const offsetX = 350  / (depth + 1);
+    // Keeps the tree compact and readable, decreases horizontal spacing as depth increases
+
+    const offsetY = 100;
+
+    createNode(
+      x - offsetX,
+      y + offsetY,
+      depth + 1,
+      id
+    );
+
+    createNode(
+      x + offsetX,
+      y + offsetY,
+      depth + 1,
+      id
+    );
+
+   
+  }
+
+   
+    createNode(
+      300,
+      50,
+      0
+    );
+
+  console.log(nodes, edges)
+  return {nodes, edges}
+}
+
+
 function graphGen(nodeCount = 6) {
 
     const nodes = []
@@ -58,16 +125,28 @@ function graphGen(nodeCount = 6) {
 }
 
 
+
+
+
 function App() {
+
+  const onNodesChange = useCallback(
+  (changes) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot), [],)
+);
+
+const onEdgesChange = useCallback(
+  (changes) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot), [])
+);
+
   const [edges, setEdges] = useState([]);
   const [nodes, setNodes] = useState([]);
   return (
 
     
-     <div style={{ height: '100%', width: '100%' }}>
+     <div style={{ height: '100vh', width: '100vw' }} className='container'>
 
        <button type='button' onClick={() => {
-        const {nodes, edges } = graphGen();
+        const {nodes, edges } = binaryGen();
 
         // Update state
         setNodes(nodes)
@@ -78,7 +157,13 @@ function App() {
       </button>
 
        <ReactFlowProvider>
-        <ReactFlow nodes={nodes} edges={edges} fitView>
+        <ReactFlow 
+        nodes={nodes} 
+        edges={edges} 
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        className='conatiner' 
+        fitView>
             <Background />
           <Controls />
       </ReactFlow>
