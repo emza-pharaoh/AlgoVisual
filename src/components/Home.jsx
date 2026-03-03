6d756d7554bd5b117702a8d0670d2ef29d8336e8
Emanuel <emanuelmhlanga55@gmail.com>
2026-03-03 23:34:04 +0200

    structure blueprint almost complete

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -36,10 +36,10 @@ export default function Home() {
   (changes) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)), []
 );
 
-  const [edges, setEdges] = useState([]);
-  const [nodes, setNodes] = useState([]);
-  const [algorithm, setAlgorithm] = useState([]);
-  const [graphType, setGraphType] = useState('random');
+const [edges, setEdges] = useState([]);
+const [nodes, setNodes] = useState([]);
+const [algorithm, setAlgorithm] = useState([]);
+const [graphType, setGraphType] = useState('random');
 
 
 
@@ -80,35 +80,54 @@ export default function Home() {
   return (
 
     
-  <div style={{ height: '80vh', width: '100vw' }} className='m-2 my-2'>
+<div className='min-h-screen flex flex-col bg-gray-100'>
 
-    
-
-      
-      <GraphCanvas
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-  />
-    
-
-       
-  <div className="flex flex-row justify-center shadow shadow-2xl relative h-100vh w-100vw absolute pointer-events-auto"
-        
-        >
-          
-      <ControlPanel
-          graphType={graphType}
-          setGraphType={setGraphType}
-          onGenerate={handleGenerate}
-          onBFS={handleBFS}
-          onDFS={handleDFS}
-          onDijkstra={handleDijkstra}
-    />
-      
+{/* NAVBAR Desktop Only */}
+    <div className="hidden lg:block border shadow shadow-2xl h-10 py-5">
+      NAVBAR
     </div>
+
+
+{/* Main Content */}
+<main className='flex-1 p-4 lg:p-8 max-w,7xl mx-auto w-full'>
+  {/* desription Panel */}
+    <div className="mb-6 border ">
+      Description Panel
+    </div>
+
+{/* Workspace */}
+    <section className='grid grid-cols-1
+                        lg:grid-cols-3
+                        gap-6'>
+
+    <div className="lg:col-span-2 min-h-100">
+    
+      <GraphCanvas
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+  />
+    </div>
+
+    <div className="hidden lg:block border">
+      Code Panel
+    </div>
+
+    </section>
+</main>
+
+   
+  
+ <ControlPanel
+        graphType={graphType}
+        setGraphType={setGraphType}
+        onGenerate={handleGenerate}
+        onBFS={handleBFS}
+        onDFS={handleDFS}
+        onDijkstra={handleDijkstra}
+    />
 
     </div>
   )
